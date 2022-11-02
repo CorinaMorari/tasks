b54059a3f9e300597cb55247f0564d00ad2ed15e
Corina <morarik8@gmail.com>
2022-11-02 19:58:20 +0200

    Add delete item function and clear all

--- assets/scripts/Aram_script.js
+++ assets/scripts/Aram_script.js
@@ -1,17 +1,32 @@
-const boxTask = document.querySelector('.input_task');
+const ulTasks = document.querySelector('.list_tasks')
 const buttonAddTask = document.querySelector('.btn_add');
 
+buttonAddTask.addEventListener('click', () => {
 
+    const text = document.querySelector('.input_task').value;
 
-buttonAddTask.addEventListener('click',()=>{
-  let text = document.querySelector('.input_task').value;
-  if (text === "") {
-    alert("Write some text!");
-  }else {
-    const li = document.createElement('li')
-    ulTasks.append(li)
-    allTasks.push(text)
-    document.querySelector('.input_task').value = '';
-    li.innerHTML = `${text}<button class="remove"> X </button>`;
-  }
+    if (text === "") {
+        alert("Write some text!");
+    } else {
+        const li = document.createElement('li');
+        const clearBtn = document.querySelector(".btn_clear_all");
+        li.innerHTML = `${text}<button class="remove"> X </button>`;
+        ulTasks.append(li);
+        document.querySelector('.input_task').value = '';
+
+        clearBtn.addEventListener("click", () => {
+          li.remove();
+        });
+    }
+
+    const li = document.querySelectorAll('li')
+    const btn = document.querySelectorAll('.remove')
+    for (let i = 0; i < li.length; i++) {
+        if (li.length > 0) {
+            btn[i].addEventListener('click', () => {
+              li[i].remove();
+            })
+        }
+    }
+
 })
