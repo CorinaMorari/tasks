fad27bf2d03848c573e7f2a05edb26fc996a54c4
Corina <morarik8@gmail.com>
2022-11-03 15:31:02 +0200

    Changed code

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,62 +1,63 @@
-const olTasks = document.querySelector('.list_tasks')
-const buttonAddTask = document.querySelector('.btn_add');
-const filterTasks = document.querySelector('.filter_task');
+const allTasks = [];
+const addButton = document.querySelector('.btn_add');
+const olTasks = document.querySelector('.list_tasks');
 
-
-buttonAddTask.addEventListener('click',()=>{
+addButton.addEventListener('click', ()=>{
     let text = document.querySelector('.input_task').value;
-    if (text === "") {
+    if (text.trim() === ''){
         alert("Write some text!");
-    }else {
-        const li = document.createElement('li');
-        const removeButton = document.createElement('button');
-        const dateSpan = document.createElement('span');
-        removeButton.classList.add('remove');
-        removeButton.textContent = 'X';
-        dateSpan.classList.add('date');
-        const getTime = new Date();
-        const date = getTime.getDate();
-        const month = getTime.getMonth()+1;
-        const year = getTime.getFullYear();
-        const hour = getTime.getHours();
-        const minutes = getTime.getMinutes();
-        dateSpan.textContent = `${date}-${month}-${year} ${hour}:${minutes}`;
-        olTasks.append(li)
-        document.querySelector('.input_task').value = '';
-        li.textContent = text;
-        li.appendChild(removeButton);
-        li.appendChild(dateSpan);
+    }else{
+        allTasks.push(text);
+        createLi(text);
+        // console.log(allTasks)
     }
-    const li = document.querySelectorAll('li')
-    const btn = document.querySelectorAll('.remove')
-    const btn_clear_all = document.querySelector('.btn_clear_all')
-    for (let i = 0; i <li.length ; i++) {
-        if(li.length > 0){
-            btn[i].addEventListener('click',()=>{
-                li[i].remove();
-            })
+    const arrayLi = document.querySelectorAll('li');
+    removeLi(arrayLi)
+    clearAll(arrayLi)
+});
 
-            btn_clear_all.addEventListener('click',()=>{
-                li[i].remove();
-            })
+const createLi = (e) => {
+    const li = document.createElement("li");
+    const removeButton = document.createElement("button");
+    const dateSpan = document.createElement("span");
+    removeButton.classList.add("remove");
+    removeButton.textContent = "X";
+    dateSpan.classList.add("date");
+    const getTime = new Date();
+    const date = getTime.getDate();
+    const month = getTime.getMonth() + 1;
+    const year = getTime.getFullYear();
+    const hour = getTime.getHours();
+    let minutes;
+    if (getTime.getMinutes() < 10) {
+        minutes = `0${getTime.getMinutes()}`
+    } else {
+        minutes = getTime.getMinutes();
+    }
+    dateSpan.textContent = `${date}-${month}-${year} ${hour}:${minutes}`;
+    olTasks.append(li);
+    document.querySelector(".input_task").value = "";
+    li.textContent = e;
+    li.appendChild(removeButton);
+    li.appendChild(dateSpan);
+};
+
+
+const removeLi = (e) => {
+    const btn = document.querySelectorAll(".remove");
+    for (let i = 0; i < e.length; i++) {
+        if (e.length > 0) {
+            btn[i].addEventListener("click", () => {
+                e[i].remove();
+            });
         }
     }
-    filterTasks.addEventListener('input', (ev)=> {
-        const filterValue = ev.target.value.toLowerCase().trim();
-        console.log(filterValue);
-        li.forEach(item =>{
-            console.log(item.textContent)
-            let itemValue = item.textContent.toLowerCase();
-            itemValue = itemValue.substring(0, itemValue.length-1);
-            console.log(itemValue);
+};
 
-            if (itemValue.includes(filterValue)){
-                item.classList.remove('none')
-            }else {
-                item.classList.add('none')
-            }
-
-        });
-
-    })
-})
+const clearAll = (e) => {
+    const clearBtn = document.querySelector(".btn_clear_all");
+    clearBtn.addEventListener("click", () => {
+        for (let i = 0; i < e.length; i++)
+            e[i].remove();
+    });
+};
