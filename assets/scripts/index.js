8de694f91f50ed7f1c58057926af50d38d3da8c8
Corina <morarik8@gmail.com>
2022-11-04 00:20:01 +0200

    Add time-stamp

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,9 +2,10 @@ const allTasks = [];
 const addButton = document.querySelector(".btn_add");
 const olTasks = document.querySelector(".list_tasks");
 const filter = document.querySelector(".filter_task");
-let arrayLi;
 
+let arrayLi;
 let taskCount = 0;
+let filteredElements = [];
 
 addButton.addEventListener("click", () => {
 
@@ -12,18 +13,35 @@ addButton.addEventListener("click", () => {
     if (text.trim() === "") {
         alert("Write some text!");
     } else {
+        const getTime = new Date();
+        let date;
+        if (getTime.getDate() < 10) {
+            date = "0" + getTime.getDate();
+        } else {
+            date = getTime.getDate();
+        }
+        const month = getTime.getMonth() + 1;
+        const year = getTime.getFullYear();
+        const hour = getTime.getHours();
+        let minutes;
+        if (getTime.getMinutes() < 10) {
+            minutes = "0" + getTime.getMinutes();
+        } else {
+            minutes = getTime.getMinutes();
+        }
+        const time = `${date}-${month}-${year} ${hour}:${minutes}`;
         taskCount += 1;
-        allTasks.push({text, index: taskCount});
-        createLi(text, taskCount);
-        console.log(allTasks);
+        allTasks.push({text, index: taskCount, time});
+        createLi(text, taskCount, time);
     }
-    arrayLi = document.querySelectorAll("li");
 });
 
-const createLi = (textValueInput, index) => {
+const createLi = (textValueInput, index, time) => {
     const li = document.createElement("li");
     const removeButton = document.createElement("button");
-    // const dateSpan = document.createElement("span");
+    const dateSpan = document.createElement("span");
+
+    dateSpan.classList.add('date')
     removeButton.classList.add("remove");
 
     removeButton.addEventListener("click", () => {
@@ -37,48 +55,30 @@ const createLi = (textValueInput, index) => {
     });
 
     removeButton.textContent = "X";
-    // dateSpan.classList.add("date");
-    // const getTime = new Date();
-    // let date;
-    // if (getTime.getDate() < 10) {
-    //     date = "0" + getTime.getDate();
-    // } else {
-    //     date = getTime.getDate();
-    // }
-    // const month = getTime.getMonth() + 1;
-    // const year = getTime.getFullYear();
-    // const hour = getTime.getHours();
-    // let minutes;
-    // if (getTime.getMinutes() < 10) {
-    //     minutes = "0" + getTime.getMinutes();
-    // } else {
-    //     minutes = getTime.getMinutes();
-    // }
-    // dateSpan.textContent = `${date}-${month}-${year} ${hour}:${minutes}`;
+    dateSpan.textContent = time;
+    li.textContent = textValueInput;
+
     olTasks.append(li);
     document.querySelector(".input_task").value = "";
-    li.textContent = textValueInput;
     li.appendChild(removeButton);
-    // li.appendChild(dateSpan);
+    li.appendChild(dateSpan);
 };
 
-
-let filteredElements = [];
 filter.addEventListener("input", (ev) => {
     if (allTasks.length > 0) {
         const filterValue = ev.target.value.toLowerCase().trim();
         filteredElements = [];
         if (filterValue !== '') {
-            for (let i = 0 ;i < allTasks.length; i++) {
+            for (let i = 0; i < allTasks.length; i++) {
                 const itemValue = allTasks[i].text.trim().toLowerCase();
                 if (itemValue.includes(filterValue)) {
-                    filteredElements.push({text: allTasks[i].text, index: allTasks[i].index})
+                    filteredElements.push({text: allTasks[i].text, index: allTasks[i].index, time: allTasks[i].time})
                     let count = 0;
-                    filteredElements.forEach(item =>{
+                    filteredElements.forEach(item => {
 
-                        if(allTasks[i].index === item.index){
+                        if (allTasks[i].index === item.index) {
                             count++
-                            if(count > 1){
+                            if (count > 1) {
                                 filteredElements.pop();
                             }
                         }
@@ -86,15 +86,14 @@ filter.addEventListener("input", (ev) => {
 
                 }
             }
-            //Got filtredElements
-            console.log("Filtred elements",filteredElements)
+
             //Show flirted elements
             arrayLi = document.querySelectorAll('li');
             clearAll(arrayLi);
             filteredElements.forEach(item => {
-                createLi(item.text, item.index)
+                createLi(item.text, item.index, item.time)
             })
-            filteredElements = []
+            filteredElements = [];
 
         } else {
 
@@ -103,7 +102,7 @@ filter.addEventListener("input", (ev) => {
             console.log(arrayLi);
 
             allTasks.forEach(item => {
-                createLi(item.text, item.index)
+                createLi(item.text, item.index, item.time)
             })
         }
 
