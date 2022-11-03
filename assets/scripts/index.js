5c05e47bbb6d4857e100848f99cd1b413bc47037
Corina <morarik8@gmail.com>
2022-11-03 23:55:06 +0200

    Done

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,63 +1,126 @@
 const allTasks = [];
-const addButton = document.querySelector('.btn_add');
-const olTasks = document.querySelector('.list_tasks');
+const addButton = document.querySelector(".btn_add");
+const olTasks = document.querySelector(".list_tasks");
+const filter = document.querySelector(".filter_task");
+let arrayLi;
 
-addButton.addEventListener('click', ()=>{
-    let text = document.querySelector('.input_task').value;
-    if (text.trim() === ''){
+let taskCount = 0;
+
+addButton.addEventListener("click", () => {
+
+    let text = document.querySelector(".input_task").value;
+    if (text.trim() === "") {
         alert("Write some text!");
-    }else{
-        allTasks.push(text);
-        createLi(text);
-        // console.log(allTasks)
+    } else {
+        taskCount += 1;
+        allTasks.push({text, index: taskCount});
+        createLi(text, taskCount);
+        console.log(allTasks);
     }
-    const arrayLi = document.querySelectorAll('li');
-    removeLi(arrayLi)
-    clearAll(arrayLi)
+    arrayLi = document.querySelectorAll("li");
 });
 
-const createLi = (e) => {
+const createLi = (textValueInput, index) => {
     const li = document.createElement("li");
     const removeButton = document.createElement("button");
-    const dateSpan = document.createElement("span");
+    // const dateSpan = document.createElement("span");
     removeButton.classList.add("remove");
+
+    removeButton.addEventListener("click", () => {
+        const taskIndex = allTasks.findIndex((task) => task.index === index);
+        if (taskIndex == -1) {
+            return;
+        }
+
+        allTasks.splice(taskIndex, 1);
+        li.remove();
+    });
+
     removeButton.textContent = "X";
-    dateSpan.classList.add("date");
-    const getTime = new Date();
-    const date = getTime.getDate();
-    const month = getTime.getMonth() + 1;
-    const year = getTime.getFullYear();
-    const hour = getTime.getHours();
-    let minutes;
-    if (getTime.getMinutes() < 10) {
-        minutes = `0${getTime.getMinutes()}`
-    } else {
-        minutes = getTime.getMinutes();
-    }
-    dateSpan.textContent = `${date}-${month}-${year} ${hour}:${minutes}`;
+    // dateSpan.classList.add("date");
+    // const getTime = new Date();
+    // let date;
+    // if (getTime.getDate() < 10) {
+    //     date = "0" + getTime.getDate();
+    // } else {
+    //     date = getTime.getDate();
+    // }
+    // const month = getTime.getMonth() + 1;
+    // const year = getTime.getFullYear();
+    // const hour = getTime.getHours();
+    // let minutes;
+    // if (getTime.getMinutes() < 10) {
+    //     minutes = "0" + getTime.getMinutes();
+    // } else {
+    //     minutes = getTime.getMinutes();
+    // }
+    // dateSpan.textContent = `${date}-${month}-${year} ${hour}:${minutes}`;
     olTasks.append(li);
     document.querySelector(".input_task").value = "";
-    li.textContent = e;
+    li.textContent = textValueInput;
     li.appendChild(removeButton);
-    li.appendChild(dateSpan);
+    // li.appendChild(dateSpan);
 };
 
 
-const removeLi = (e) => {
-    const btn = document.querySelectorAll(".remove");
-    for (let i = 0; i < e.length; i++) {
-        if (e.length > 0) {
-            btn[i].addEventListener("click", () => {
-                e[i].remove();
-            });
+let filteredElements = [];
+filter.addEventListener("input", (ev) => {
+    if (allTasks.length > 0) {
+        const filterValue = ev.target.value.toLowerCase().trim();
+        filteredElements = [];
+        if (filterValue !== '') {
+            for (let i = 0 ;i < allTasks.length; i++) {
+                const itemValue = allTasks[i].text.trim().toLowerCase();
+                if (itemValue.includes(filterValue)) {
+                    filteredElements.push({text: allTasks[i].text, index: allTasks[i].index})
+                    let count = 0;
+                    filteredElements.forEach(item =>{
+
+                        if(allTasks[i].index === item.index){
+                            count++
+                            if(count > 1){
+                                filteredElements.pop();
+                            }
+                        }
+                    })
+
+                }
+            }
+            //Got filtredElements
+            console.log("Filtred elements",filteredElements)
+            //Show flirted elements
+            arrayLi = document.querySelectorAll('li');
+            clearAll(arrayLi);
+            filteredElements.forEach(item => {
+                createLi(item.text, item.index)
+            })
+            filteredElements = []
+
+        } else {
+
+            arrayLi = document.querySelectorAll('li');
+            clearAll(arrayLi);
+            console.log(arrayLi);
+
+            allTasks.forEach(item => {
+                createLi(item.text, item.index)
+            })
         }
-    }
-};
 
-const clearAll = (e) => {
-    const clearBtn = document.querySelector(".btn_clear_all");
-    clearBtn.addEventListener("click", () => {
-        for (let i = 0; i < e.length; i++)
-            e[i].remove();
-    });
-};
+    }
+})
+
+const clearBtn = document.querySelector(".btn_clear_all");
+clearBtn.addEventListener("click", () => {
+    arrayLi = document.querySelectorAll('li');
+    clearAll(arrayLi);
+    allTasks.splice(0);
+    console.log(allTasks);
+});
+
+
+const clearAll = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        arr[i].remove();
+    }
+}
